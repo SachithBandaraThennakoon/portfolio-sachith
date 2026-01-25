079b312a082cf7296bd9d4135894edc23b41cfdc
SachithShilshan <sachith.edu6021@gmail.com>
2026-01-25 20:08:47 +0530

    jan 25 remove #

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/globals.css';
-import { HashRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
